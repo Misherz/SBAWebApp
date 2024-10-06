9e323134859aa30918b388679bec910cee842b4f
Misherz <michelle.j.lugo@gmail.com>
2024-10-06 15:51:16 -0400

    Added ReadMe

---
+++ README.md
@@ -0,0 +1,23 @@
+# SBA Project: Javascript Web App
+### Description: 
+    Let the feeling of existential dread sink in. Also, enjoy some nice photos of Space.
+
+
+### Technologies Used :
+    HTML/CSS
+    Per Scholas Slides
+    BroCode YT: https://www.youtube.com/watch?v=lfmg-EJ8gm4
+    NASA API: https://api.nasa.gov/
+
+### How to get started :
+    Be surprised with image search function - enter any number in range to see cool photos provided by Nasa.
+
+
+### Acknowledgement :
+    BroCode YT guide
+    Nasa API
+
+
+### Github:
+    https://github.com/Misherz/SBAWebApp
+
--- script.mjs
+++ script.mjs
@@ -35,15 +35,15 @@ async function fetchData() {
 }
 
 //Fetch practice
-fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error("Could not retrieve resource");
-        }
-        return response.json();
-    })
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
+// fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
+//     .then(response => {
+//         if (!response.ok) {
+//             throw new Error("Could not retrieve resource");
+//         }
+//         return response.json();
+//     })
+//     .then(data => console.log(data))
+//     .catch(error => console.error(error));
 
 
 
@@ -58,3 +58,54 @@ function popUp() {
 }
 
 document.getElementById("guide").addEventListener("click", popUp);
+
+// GetImg Function
+
+
+async function getImg(event) {
+    event.preventDefault();
+    document.getElementById("getImageButton").addEventListener("click", getImg);
+
+    
+    const textInput = document.getElementById("textInput").value;
+
+    try {
+
+        const res = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${API_KEY}`);
+
+        if (!res.ok) {
+            throw new Error("Could not retrieve resource");
+        }
+        const getData =  await res.json();
+        console.log(getData)
+
+        if (photo) {
+            const resultImgElement = document.getElementById("nasaImgs");
+            resultImgElement.src = photo[index].img_src; 
+            resultImgElement.style.display = "block"; 
+        } else {
+            console.error("ID not found");
+            alert("Photo not found. Please check the photo ID.");
+        }
+
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
+
+
+
+//Test API photo
+
+
+fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${API_KEY}`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Could not retrieve resource");
+        }
+        return response.json();
+    })
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
