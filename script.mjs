2334ee3759faa4ada62294a42cf3cb5c987da707
Misherz <michelle.j.lugo@gmail.com>
2024-10-06 12:06:33 -0400

    images added

--- script.mjs
+++ script.mjs
@@ -6,16 +6,24 @@ const API_KEY = 'ywmjIhPygnpx0LacggNE7nqtALqLWUzEdDvsxcjI';
 
 //async and await
 fetchData()
-
 async function fetchData() {
 
     try {
+        
+
+
         const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`, { headers: { "x-api-key": API_KEY } })
         if (!response.ok) {
             throw new Error("Could not retrieve resource");
         }
-        const datas =  await response.json();
-        console.log(datas)
+        const data =  await response.json();
+        console.log(data)
+
+        const imgOfTheDay = data.url;
+        const imgElement = document.getElementById("mainPg");
+
+        imgElement.src = imgOfTheDay;
+        imgElement.style.display="block";
 
     }
     catch (error) {
