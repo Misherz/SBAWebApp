0ca30660e9ff38d862dd0cfd54d401ba799140e1
Misherz <michelle.j.lugo@gmail.com>
2024-10-06 11:37:28 -0400

    async await added

--- script.mjs
+++ script.mjs
@@ -4,18 +4,32 @@
 
 const API_KEY = 'ywmjIhPygnpx0LacggNE7nqtALqLWUzEdDvsxcjI';
 
-// (async function initialLoad() {
-//     let pic = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`, { headers: { "x-api-key": API_KEY } })
-//     let picOfTheDay =  await res.json()
-// })
+//async and await
+fetchData()
+
+async function fetchData() {
+
+    try {
+        const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`, { headers: { "x-api-key": API_KEY } })
+        if (!response.ok) {
+            throw new Error("Could not retrieve resource");
+        }
+        const datas =  await response.json();
+        console.log(datas)
+
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
 
 //Fetch practice
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
     .then(response => {
-        if(!response.ok) {
-        throw new Error("Could not retrieve resource");
-    } 
-    return response.json();
+        if (!response.ok) {
+            throw new Error("Could not retrieve resource");
+        }
+        return response.json();
     })
     .then(data => console.log(data))
     .catch(error => console.error(error));
